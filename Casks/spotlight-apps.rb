45997f187d0fcf49dac678653d135698b978e97d
Ben White <hello@bemm.com.au>
2020-07-17 16:07:16 +1000

    switch from master to main

--- Casks/spotlight-apps.rb
+++ Casks/spotlight-apps.rb
@@ -2,10 +2,10 @@ cask 'spotlight-apps' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/d3v1an7/spotlight-apps/archive/master.tar.gz'
+  url 'https://github.com/d3v1an7/spotlight-apps/archive/main.tar.gz'
   name 'spotlight-apps'
   homepage 'https://github.com/d3v1an7/spotlight-apps/'
 
-  app 'spotlight-apps-master/app/Lock.app'
-  app 'spotlight-apps-master/app/Screen Saver.app'
+  app 'spotlight-apps-main/app/Lock.app'
+  app 'spotlight-apps-main/app/Screen Saver.app'
 end
